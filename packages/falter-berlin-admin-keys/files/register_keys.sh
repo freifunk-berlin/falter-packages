48a118b2077003df7b7b9fd30d6b0d2f0bea2700
Martin Hübner <martin.hubner@web.de>
2020-12-27 17:28:38 +0100

    falter-berlin-admin-keys: add package
    This package holds the ssh keys of Freifunkers who volunteer in
    operating the backbone network. Upon Install of the package, all
    keys will be added to the authorized-keys of that router.
    
    Signed-off-by: Martin Hübner <martin.hubner@web.de>

---
+++ packages/falter-berlin-admin-keys/files/register_keys.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# register all keys in directory keys/ into the dropbears key-file
+
+SCRIPTPATH=$(dirname $(readlink -f "$0"))
+KEY_FILES=$(find "$SCRIPTPATH" -name "*.pub")
+
+for KEY_FILE in $KEY_FILES; do
+    KEY=$(cat $KEY_FILE)
+    if [ -z "$(grep "$KEY" /etc/dropbear/authorized_keys)" ]; then
+        echo "$KEY" >> /etc/dropbear/authorized_keys
+    fi
+done
