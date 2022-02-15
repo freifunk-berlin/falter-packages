12e46cc91c407f0010a0afd0a7997632d639e5a1
Nick Hainke <vincent@systemli.org>
2022-02-15 17:00:29 +0100

    falter-berlin-tunnelmanager: several fixes (#278)
    * falter-berlin-tunnelmanager: don't try to connect to same server on error
    
    If a server denies a request, tunnelmanager ignores it and always tries
    to connect to it again, if it is the server with the least users.
    
    In case a server denies a request, search again for another server.
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>
    
    * falter-berlin-tunnelmanager: replace vpnmanager with tunnelman
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>
    
    * falter-berlin-tunnelmanager: switch to ipc in up/down
    
    Switch to using ipc for the up/down scripts. Adding and removing will be
    seamless.
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>
    
    * falter-berlin-tunnelmanager: cleanup interface config before adding new interfaces
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>
    
    * falter-berlin-tunnelmanager: cleanup idents
    
    Use
        shfmt -i 4 -w $FILE
    to use spaces instead of tabs.
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>
    
    * falter-berlin-tunnelmanager: use "=" for checking strings
    
    "==" is not defined. Use "=" instead.
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>
    
    * falter-berlin-tunnelmanager: increment version
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>
    (cherry picked from commit ccd013404d658fb13205e2da9752b188d9fbc0df)

--- packages/falter-berlin-tunnelmanager/files/up.sh
+++ packages/falter-berlin-tunnelmanager/files/up.sh
@@ -19,8 +19,79 @@ get_configured_ips() {
     ip -o -4 a s | awk -F ' |\/' '{print $7}'
 }
 
-get_ips_from_prefix "$prefix"
+cleanup_olsr_wg_config() {
+    # all_olsr_interfaces=$(echo "/int" | nc 127.0.0.1 2006 | tail +3 | awk '{print $1}')
+    olsr_wg_interfaces=$(echo "/int" | nc 127.0.0.1 2006 | grep wg_ | awk '{print $1}')
 
+    i=0
+    while uci get olsrd.@Interface[$i] &>/dev/null; do
+        int_name=$(uci get olsrd.@Interface[$i].interface)
+        if [ $? -ne 0 ]; then # check if interface config is wrong
+            uci delete olsrd.@Interface[$i]
+            continue
+        fi
+
+        # skip non wireguard interfaces
+        slicedint=$(echo $int_name | cut -c1-3)
+        if [ "${slicedint}" != "wg_" ]; then
+            i=$((i + 1))
+            continue
+        fi
+
+        # shell ...
+        do_delete=1
+        for olsr_int in $olsr_wg_interfaces; do
+            if [ "$int_name" = "$olsr_int" ]; then
+                do_delete=0
+            fi
+        done
+
+        if [ $do_delete -eq 1 ]; then
+            uci delete olsrd.@Interface[$i]
+        else
+            i=$((i + 1))
+        fi
+    done
+    uci commit
+}
+
+cleanup_babel_wg_config() {
+    # all_babel_interfaces=$(echo "dump" | nc ::1 33123 | grep interface | awk '{print $3}')
+    babel_wg_interfaces=$(echo "dump" | nc ::1 33123 | grep interface | grep wg_ | awk '{print $3}')
+
+    i=0
+    while uci get babeld.@interface[$i] &>/dev/null; do
+        int_name=$(uci get babeld.@interface[$i].ifname)
+        if [ $? -ne 0 ]; then # check if interface config is wrong
+            uci delete babeld.@interface[$i]
+            continue
+        fi
+
+        # skip non wireguard interfaces
+        slicedint=$(echo $int_name | cut -c1-3)
+        if [ "${slicedint}" != "wg_" ]; then
+            i=$((i + 1))
+            continue
+        fi
+
+        # shell ...
+        do_delete=1
+        for babel_int in $babel_wg_interfaces; do
+            if [ "$int_name" = "$babel_int" ]; then
+                do_delete=0
+            fi
+        done
+
+        if [ $do_delete -eq 1 ]; then
+            uci delete babeld.@interface[$i]
+        else
+            i=$((i + 1))
+        fi
+    done
+    uci commit
+}
+
+get_ips_from_prefix "$prefix"
 
 # unconditionally wipe all configured ips from available ips.
 available_ips="$_ret_prefixes"
@@ -35,8 +106,13 @@ ip address add "$next_ip/32" dev "$interface"
 ip address add "fe80::2/64" dev "$interface"
 
 # bringup interface
-
 ip link set up dev "$interface"
+
+cleanup_olsr_wg_config
+cleanup_babel_wg_config
+
+# wait some time before bringing up olsrd and babeld
+sleep 2
 
 # Configure OLSRD
 uci revert olsrd
@@ -47,8 +123,15 @@ uci set "olsrd.$UCIREF.interface=$interface"
 uci set "olsrd.$UCIREF.Mode=ether"
 uci commit olsrd
 
-/etc/init.d/olsrd reload
-
+# check if olsrd is started and ubus is available
+ubus list | grep -qF olsrd
+if [ $? -eq 1 ]; then
+    # no olsr running start
+    /etc/init.d/olsrd start
+else
+    # instead of reloading add interface via ipc to make it seamless
+    ubus call olsrd add_interface '{"ifname":'\""$interface"\"'}'
+fi
 
 # Configure babeld
 uci revert babeld
@@ -57,4 +140,11 @@ uci set "babeld.$UCIREF.ifname=$interface"
 uci set "babeld.$UCIREF.split_horizon=true"
 uci commit babeld
 
-/etc/init.d/babeld reload
+# check if babeld is started and ubus is available
+ubus list | grep -qF babeld
+if [ $? -eq 1 ]; then
+    /etc/init.d/babeld start
+else
+    # instead of reloading add interface via ipc to make it seamless
+    ubus call babeld add_interface '{"ifname":'\""$interface"\"'}'
+fi
