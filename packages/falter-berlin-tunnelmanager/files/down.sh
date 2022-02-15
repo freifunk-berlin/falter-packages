ccd013404d658fb13205e2da9752b188d9fbc0df
Nick Hainke <vincent@systemli.org>
2022-02-15 17:00:29 +0100

    falter-berlin-tunnelmanager: several fixes (#278)
    * falter-berlin-tunnelmanager: don't try to connect to same server on error
    
    If a server denies a request, tunnelmanager ignores it and always tries
    to connect to it again, if it is the server with the least users.
    
    In case a server denies a request, search again for another server.
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>
    
    * falter-berlin-tunnelmanager: replace vpnmanager with tunnelman
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>
    
    * falter-berlin-tunnelmanager: switch to ipc in up/down
    
    Switch to using ipc for the up/down scripts. Adding and removing will be
    seamless.
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>
    
    * falter-berlin-tunnelmanager: cleanup interface config before adding new interfaces
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>
    
    * falter-berlin-tunnelmanager: cleanup idents
    
    Use
        shfmt -i 4 -w $FILE
    to use spaces instead of tabs.
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>
    
    * falter-berlin-tunnelmanager: use "=" for checking strings
    
    "==" is not defined. Use "=" instead.
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>
    
    * falter-berlin-tunnelmanager: increment version
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>

--- packages/falter-berlin-tunnelmanager/files/down.sh
+++ packages/falter-berlin-tunnelmanager/files/down.sh
@@ -2,13 +2,13 @@
 
 interface="$1"
 
-
-
 uci revert olsrd
 section="$(uci show olsrd | grep $interface | awk -F '.interface' '{print $1}')"
 uci delete "$section"
 uci commit olsrd
-/etc/init.d/olsrd reload
+
+# delete interface via ipc instead of reloading to make it seamless
+ubus call olsrd del_interface '{"ifname":'\""$interface"\"'}'
 
 uci revert babeld
 section="$(uci show babeld | grep $interface | awk -F '.ifname' '{print $1}')"
