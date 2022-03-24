ed541ebe120ea0229a61d5b8506f27df91abd385
Martin Hübner <martin.hubner@web.de>
2022-03-24 22:21:41 +0100

    falter-berlin-autoupdate: add some checks and refinements
    This adds some further checks. For example, nodes with development
    firmware shouldn't perfom an auto-update. Reorder checks.
    
    Signed-off-by: Martin Hübner <martin.hubner@web.de>

--- packages/falter-berlin-autoupdate/files/post-inst.sh
+++ packages/falter-berlin-autoupdate/files/post-inst.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
 
-#if autoupdate is not present in crontab, include it.
-crontab -l | grep /usr/bin/autoupdate >> /dev/null
+# if autoupdate is not present in crontab, include it.
+crontab -l | grep /usr/bin/autoupdate >>/dev/null
 if [ $? != 0 ]; then
-    #get a fairly random update-time, to protect the servers from DoS
-    HOUR=$(($(tr -cd 0-9 </dev/urandom | head -c 2) % 24))
+    # get a fairly random update-time, to protect the servers from DoS. Will be something between 3 and 5 a.m.
+    HOUR=$((($(tr -cd 0-9 </dev/urandom | head -c 2) % 3) + 3))
     MIN=$(($(tr -cd 0-9 </dev/urandom | head -c 2) % 60))
-    echo "$MIN $HOUR * * *        /usr/bin/autoupdate" >> /etc/crontabs/root
-    
-	/etc/init.d/cron restart
+    echo "$MIN $HOUR * * *        /usr/bin/autoupdate" >>/etc/crontabs/root
+
+    /etc/init.d/cron restart
 fi
