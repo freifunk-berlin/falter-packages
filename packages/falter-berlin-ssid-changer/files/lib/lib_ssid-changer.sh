91fac15655291691f036897e3bd8bf2c510e302a
Martin Hübner <martin.hubner@web.de>
2022-08-28 23:19:57 +0200

    falter-berlin-ssid-changer: use dns server from config
    Instead of hardcoding the dns servers of big dns providers, we should use
    the dns servers configured for checking internet connectivity.
    This prevents the case, where clients can't reach the internet, as these
    servers are not reachable, but the big servers are.
    
    Applies some shellcheck-issues additionally.
    
    Fixes #309
    
    Signed-off-by: Martin Hübner <martin.hubner@web.de>
    (cherry picked from commit 56bab5f53d8ad22936d857d0c2e510346a009e56)

--- packages/falter-berlin-ssid-changer/files/lib/lib_ssid-changer.sh
+++ packages/falter-berlin-ssid-changer/files/lib/lib_ssid-changer.sh
@@ -1,11 +1,14 @@
 #/bin/sh
+
+# shellcheck shell=dash
+# shellcheck disable=SC2155
+
 . /lib/functions.sh
 . /lib/functions/network.sh
 
-DNS_SERVER="8.8.8.8 1.1.1.1 9.9.9.9"
-NODENAME=$(uci_get system @system[-1] hostname | cut -b -24 ) # cut nodename to not exceed 32 bytes
-OFFLINE_SSID="offline_""$NODENAME"
-
+DNS_SERVER=$(uci_get network loopback dns)
+NODENAME=$(uci_get system @system[-1] hostname | cut -b -24) # cut nodename to not exceed 32 bytes
+export OFFLINE_SSID="offline_""$NODENAME"
 
 log() {
     logger -s -t "ssid-changer" -p 5 "$1"
@@ -14,17 +17,16 @@ log() {
 increment_ip_addr() {
     local raw_addr="$1"
 
-    local net=$(echo $raw_addr | cut -d'.' -f -3)
-    local host=$(echo $raw_addr | cut -d'.' -f 4)
-    host=$(( $host + 1 ))
+    local net=$(echo "$raw_addr" | cut -d'.' -f -3)
+    local host=$(echo "$raw_addr" | cut -d'.' -f 4)
+    host=$((host + 1))
     echo "$net"".$host"
 }
 
 is_internet_reachable_via_ffuplink() {
     # check if clients could reach the internet via ffuplink (only exists if we have a wan port)
     for SERVER in $DNS_SERVER; do
-        ping -c1 -W 3 -I ffuplink $SERVER > /dev/null
-        if [ $? = 0 ]; then
+        if ping -c1 -W 3 -I ffuplink "$SERVER" >/dev/null; then
             return 0
         fi
     done
@@ -35,10 +37,9 @@ exists_route_over_mesh() {
     # if internet not reachable via ffuplink, is there no route over the mesh either?
     local dhcp
     network_get_ipaddr dhcp dhcp
-    dhcp=$(increment_ip_addr $dhcp)
+    dhcp=$(increment_ip_addr "$dhcp")
     for SERVER in $DNS_SERVER; do
-        ip r g $SERVER from $dhcp iif br-dhcp
-        if [ $? = 0 ]; then # theres a route to the internet.
+        if ip r g "$SERVER" from "$dhcp" iif br-dhcp; then # theres a route to the internet.
             return 0
         fi
     done
@@ -59,7 +60,7 @@ is_internet_reachable() {
 get_interfaces() {
     # get the names of every interface named *dhcp* and fetch its normal ssid. Thus we get
     # 2.4 and/or 5 GHz both
-    local IFACES=$(uci show wireless | grep -e "dhcp.*\.ssid='.*\.freifunk.net'" | cut -d'=' -f1 )
+    local IFACES=$(uci show wireless | grep -e "dhcp.*\.ssid='.*\.freifunk.net'" | cut -d'=' -f1)
     local ONLINE_SSIDS=""
     for IFACE in $IFACES; do
         local SSID=$(uci_get "$IFACE")
