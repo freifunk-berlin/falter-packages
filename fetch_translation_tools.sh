d2113dfca56d1d31941cdf77ee4d25dbea87012f
Martin Hübner <martin.hubner@web.de>
2022-08-29 12:42:54 +0200

    fetch_translation_tools.sh: fix shellcheck issues
    This commit fixes some shellcheck issues in the mentioned script.
    It precedes #315.
    
    Signed-off-by: Martin Hübner <martin.hubner@web.de>

--- fetch_translation_tools.sh
+++ fetch_translation_tools.sh
@@ -2,16 +2,16 @@
 
 # fetch the translation-tools from the luci repo
 
-LUCI_REPO="https://github.com/openwrt/luci.git"
 LUCI_ZIP="https://github.com/openwrt/luci/archive/refs/heads/master.zip"
 
 DIR="_tmp_"
 
 git clean -df build/
 mkdir $DIR
-cd $DIR
-wget "$LUCI_ZIP"
-unzip "master.zip"
-mv luci-master/build/* ../build
-cd ..
+(
+    cd $DIR || exit 2
+    wget "$LUCI_ZIP"
+    unzip "master.zip"
+    mv luci-master/build ..
+)
 rm -rf $DIR
