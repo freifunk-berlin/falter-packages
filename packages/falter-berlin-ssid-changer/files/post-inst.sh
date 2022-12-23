b1614c533056e245217e84ee65de45520c8c57ea
Martin Hübner <martin.hubner@web.de>
2022-12-23 20:32:23 +0100

    falter-berlin-ssid-changer: several fixes
    In the merged state before, the ssid-changer was only functional, if there
    did happen a change in the state of interfaces. But if a router started
    without having internet connectivity, it wasn't working at all.
    
    This commit adds a check for connectivity running every 5 minutes, hopefully
    increasing the user experience a lot.
    
    Signed-off-by: Martin Hübner <martin.hubner@web.de>

---
+++ packages/falter-berlin-ssid-changer/files/post-inst.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+
+# if ssid-changer is not present in crontab, include it.
+crontab -l | grep ssid-changer >>/dev/null
+if [ $? != 0 ]; then
+    # check every 5 minutes, if we can still reach the internet, but delay
+    # execution randomly, to achieve evenly distribution
+    echo '# check every 5 minutes, if we can still reach the internet, but delay execution randomly, to achieve evenly distribution' >>/etc/crontabs/root
+    echo '*/5 * * * *      test -e /etc/hotplug.d/net/30-ssid-changer && DELAY=some INTERFACE=ffuplink sh /etc/hotplug.d/net/30-ssid-changer' >>/etc/crontabs/root
+
+    /etc/init.d/cron restart
+fi
