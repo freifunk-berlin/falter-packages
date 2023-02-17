ef7e988a3d30907d34d1ffceac8dafeb6f786158
Martin Hübner <martin.hubner@web.de>
2023-02-17 18:26:47 +0100

    falter-berlin-migration: fix shellcheck for semver.sh
    Signed-off-by: Martin Hübner <martin.hubner@web.de>
    (cherry picked from commit bd63bb1a8e2bab6fadaa7f5e01fb687fe01bbf2a)

--- packages/falter-berlin-migration/files/lib/functions/semver.sh
+++ packages/falter-berlin-migration/files/lib/functions/semver.sh
@@ -30,16 +30,19 @@
 # Modified for recent best practicies on shells
 # 2023, by Freifunk Berlin
 
+# shellcheck shell=dash
+# shellcheck disable=SC3010
+
 semverParseInto() {
     local RE='[^0-9]*\([0-9]*\)[.]\([0-9]*\)[.]\([0-9]*\)\([0-9A-Za-z-]*\)'
     #MAJOR
-    eval $2="$(echo "$1" | sed -e "s#$RE#\1#")"
+    eval "$2"="$(echo "$1" | sed -e "s#$RE#\1#")"
     #MINOR
-    eval $3="$(echo "$1" | sed -e "s#$RE#\2#")"
+    eval "$3"="$(echo "$1" | sed -e "s#$RE#\2#")"
     #MINOR
-    eval $4="$(echo "$1" | sed -e "s#$RE#\3#")"
+    eval "$4"="$(echo "$1" | sed -e "s#$RE#\3#")"
     #SPECIAL
-    eval $5="$(echo "$1" | sed -e "s#$RE#\4#")"
+    eval "$5"="$(echo "$1" | sed -e "s#$RE#\4#")"
 }
 
 semverEQ() {
