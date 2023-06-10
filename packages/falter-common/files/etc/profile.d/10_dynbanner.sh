608c30d5b86febfc2ac0d7dccc09cc2f4cce2609
Martin Hübner <martin.hubner@web.de>
2023-06-10 12:36:07 +0200

    falter-common: apply shellcheck-fixes
    exclude hna.lua from linter.
    
    Signed-off-by: Martin Hübner <martin.hubner@web.de>

--- packages/falter-common/files/etc/profile.d/10_dynbanner.sh
+++ packages/falter-common/files/etc/profile.d/10_dynbanner.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+# shellcheck shell=bash
+# shellcheck disable=SC2059
+
 # Get dynamic information and print them under the banner.
+#
+# This script originates from Freifunk Berlin
 
 HOSTNAME=$(uci -q get system.@system[0].hostname)".olsr"
 IPADDR=$(uci -q get network.dhcp.ipaddr)
 UPTIME=$(uptime | cut -d ',' -f 0 | cut -d ' ' -f 4-) 2&> /dev/null
 FREEFL=$(df -h | grep " /overlay" | sed -E -e s/[[:space:]]+/\;/g | cut -d';' -f4 ) 2&> /dev/null
-SYS_LOAD=$(cat /proc/loadavg | cut -d' ' -f 1-3) 2&> /dev/null
+SYS_LOAD=$(cut -d' ' -f 1-3 < /proc/loadavg ) 2&> /dev/null
 CLIENTS=$(wc -l /tmp/dhcp.leases | cut -d' ' -f1) 2&> /dev/null
 
 printf \
--- packages/falter-common/files/usr/bin/watch.sh
+++ packages/falter-common/files/usr/bin/watch.sh
@@ -2,10 +2,10 @@
 command="$1"
 interval="$2"; [ -z "$interval" ] && interval=1
 
-if [ -z $command ]; then
+if [ -z "$command" ]; then
         echo 'Usage: watch.sh "command [options]" [interval], e.g. watch "ifconfig ath0" 2'
         echo 'interval is optional and defaults to 1'
         exit 1
 fi
 
-while true; do clear; $command; sleep $interval; done
+while true; do clear; $command; sleep "$interval"; done
