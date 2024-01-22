f421b74c6ca1f7039e322b3edee39a7b8a1c897c
Packet Please <pktpls@systemli.org>
2024-01-23 00:44:27 +0100

    cleanup: remove OpenVPN leftovers
    Signed-off-by: Packet Please <pktpls@systemli.org>

--- luci/luci-app-ffwizard-falter/luasrc/model/cbi/freifunk/assistent/shareInternet.lua
+++ luci/luci-app-ffwizard-falter/luasrc/model/cbi/freifunk/assistent/shareInternet.lua
@@ -27,31 +27,7 @@ function usersBandwidthUp.cfgvalue(self, section)
   return uci:get("ffwizard", "settings", "usersBandwidthUp")
 end
 
-if uci:get("ffberlin-uplink", "uplink", "auth") == "x509" then
-  vpninfo = f:field(DummyValue, "vpninfo", "")
-  vpninfo.template = "freifunk/assistent/snippets/vpninfo"
-  if luci.http.formvalue("reupload", true) == "1" then
-    fs.unlink("/etc/openvpn/ffuplink.crt")
-    fs.unlink("/etc/openvpn/ffuplink.key")
-    luci.http.redirect(luci.dispatcher.build_url("admin/freifunk/assistent/sharedInternet"))
-  end
-  if fs.access("/etc/openvpn/ffuplink.crt") then
-    vpncertreupload = f:field(DummyValue, "reupload", "")
-    vpncertreupload.template = "freifunk/assistent/snippets/vpncertreupload"
-  else
-    local cert = f:field(FileUpload, "cert", translate("Local Certificate"),"ffuplink.crt")
-    cert.default="/etc/openvpn/ffuplink.crt"
-    cert.rmempty = false
-    cert.optional = false
-
-    local key = f:field(FileUpload, "key", translate("Local Key"),"ffuplink.key")
-    key.default="/etc/openvpn/ffuplink.key"
-    key.rmempty = false
-    key.optional = false
-  end
-end
-
-main = f:field(DummyValue, "openvpnconfig", "", "")
+main = f:field(DummyValue, "shareInternet", "", "")
 main.forcewrite = true
 
 function main.parse(self, section)
@@ -66,26 +42,7 @@ function main.write(self, section, value)
   uci:set("ffwizard", "settings", "usersBandwidthUp", usersBandwidthUp:formvalue(section))
   uci:set("ffwizard", "settings", "usersBandwidthDown", usersBandwidthDown:formvalue(section))
 
-  uci:section("openvpn", "openvpn", "ffuplink", {
-    --persist_tun='0',
-    enabled='1'
-  })
-
-  fs.move(
-    "/etc/luci-uploads/cbid.ffuplink.1.cert",
-    "/etc/openvpn/ffuplink.crt"
-  )
-  fs.move(
-    "/etc/luci-uploads/cbid.ffuplink.1.key",
-    "/etc/openvpn/ffuplink.key"
-  )
-
-  uci:save("openvpn")
   uci:save("ffwizard")
-
-  -- I need to commit this here, don't know why I can not do this in apply changes
-  uci:commit("openvpn")
-
 end
 
 function f.handle(self, state, data)
