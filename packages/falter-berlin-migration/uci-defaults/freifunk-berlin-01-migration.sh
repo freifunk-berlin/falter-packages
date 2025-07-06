365ca3694e60053835709146c2accde4ee2db6aa
Martin Hübner <martin.hubner@web.de>
2025-07-06 18:07:57 +0200

    falter-berlin-migration: Remove ns2.digitalcourage.de
    Signed-off-by: Martin Hübner <martin.hubner@web.de>

--- packages/falter-berlin-migration/uci-defaults/freifunk-berlin-01-migration.sh
+++ packages/falter-berlin-migration/uci-defaults/freifunk-berlin-01-migration.sh
@@ -1057,7 +1057,7 @@ r1_2_3_update_owm_cron() {
 
 r1_4_1_update_dns() {
     log "updating dns servers in network-config"
-    uci set network.loopback.dns='194.150.168.168 9.9.9.10 149.112.112.10 2a02:2970:1002::18 2620:fe::10 2620:fe::fe:10'
+    uci set network.loopback.dns='194.150.168.168 9.9.9.10 149.112.112.10 2620:fe::10 2620:fe::fe:10'
     uci commit network
     log "updating dns-servers in freifunk-file"
     uci set freifunk.interface.dns='9.9.9.10 149.112.112.10'
