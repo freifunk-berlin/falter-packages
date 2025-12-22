21628af83b0be8849257160998dbaa01c5c96088
Packet Please <pktpls@systemli.org>
2025-12-22 20:54:49 +0100

    autoupdate: remove the cleanup trap, it's racey
    Starting with OpenWrt 22.03, it looks like it deletes the image
    while sysupgrade still needs the file. The result is that autoupdater
    output looks completely normal, but the device reboots into the old
    firmware.
    
    The cleanup isn't strictly neccessary, so let's remove it for now.
    
    Signed-off-by: Packet Please <pktpls@systemli.org>
    (cherry picked from commit b5deff8f8542739da20a1da9c772e4b54ee894d7)

--- packages/falter-berlin-autoupdate/files/autoupdate.sh
+++ packages/falter-berlin-autoupdate/files/autoupdate.sh
@@ -21,17 +21,6 @@
 . /lib/config/uci.sh
 . /etc/freifunk_release
 
-cleanup() {
-    log "exiting..."
-
-    if [ -d "$PATH_DIR" ] && [ -z "$OPT_TESTRUN" ]; then
-        rm -rf "$PATH_DIR"
-    fi
-
-    exit
-}
-trap cleanup EXIT
-
 print_help() {
     printf "\
 
@@ -246,7 +235,6 @@ if semverLT "$FREIFUNK_RELEASE" "$latest_release"; then
                 sysupgrade "$PATH_BIN"
             fi
         fi
-        log "done."
     fi
 else
     log "$FREIFUNK_RELEASE is the latest version. Nothing to do. I will recheck tomorrow."
