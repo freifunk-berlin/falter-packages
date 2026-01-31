357bb022ba004e55cdb5b5afee052e3ea3b3227a
Packet Please <pktpls@systemli.org>
2026-01-31 02:17:47 +0100

    Revert "build: disable shallow cloning"
    This reverts commit 713764d8a7ec11f52b98d5df3d030f82c48f8c74.
    
    Signed-off-by: Packet Please <pktpls@systemli.org>

--- build/build.sh
+++ build/build.sh
@@ -127,9 +127,6 @@ EOF
 
   cd "$sdkdir"
 
-  # disable shallow cloning, so the http mirrors can be used
-  sed -i 's/--depth 1//;s/--depth=1//' scripts/feeds
-
   # build a repository from all packages in our feed
   if [ "$branch" != "main" ] && [ "$branch" != "openwrt-25.12" ] ; then
     echo "CONFIG_SIGNED_PACKAGES=n" > .config
