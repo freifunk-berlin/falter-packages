afa36d7f3c40dd4457366ca00842c2f57717b272
Perry Melange <isprotejesvalkata@gmail.com>
2026-05-28 11:01:16 +0200

    falter-common: 10_dynbanner dynamic domain name from config
    remove hard coded olsr suffix and replace it with either the
    community profile suffix or a default of "ff"
    
    Signed-off-by: Perry Melange <isprotejesvalkata@gmail.com>
    (cherry picked from commit 3520b83a8c50aea68075d79ad926abeacfc48e07)
    (cherry picked from commit b4ad2a3cf067903ad91e7769ceaa65ec4ecfacc3)

---
+++ packages/falter-common/files/etc/profile.d/10_dynbanner.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# This script originates from Freifunk Berlin. It gets dynamic information
+# and prints them for user information below the banner.
+# It is licensed under GNU General Public License v3.0 or later
+# Copyright (C) 2021   Martin Hübner
+
+# shellcheck shell=dash
+
+COMMUNITY=$(uci -q get freifunk.community.name)
+SUFFIX=$(uci -q get profile_${COMMUNITY}.profile.suffix)
+if [ 1 -eq $? ]; then
+  SUFFIX="ff"
+fi
+HOSTNAME=$(uci -q get system.@system[0].hostname)".${SUFFIX}"
+IPADDR=$(uci -q get network.dhcp.ipaddr)
+UPTIME=$(uptime | cut -d ',' -f 0 | cut -d ' ' -f 4-) >/dev/null 2>&1
+FREEFL=$(df -h | grep " /overlay" | sed -E -e s/[[:space:]]+/\;/g | cut -d';' -f4) >/dev/null 2>&1
+SYS_LOAD=$(cut -d' ' -f 1-3 </proc/loadavg) >/dev/null 2>&1
+CLIENTS=$(wc -l /tmp/dhcp.leases | cut -d' ' -f1) >/dev/null 2>&1
+
+printf \
+    " Host.............................: %s
+ IP-Address.......................: %s
+ Uptime...........................: %s
+ Free flash.......................: %s
+ Average load (1m, 5m, 15m).......: %s
+ DHCP-Clients.....................: %s
+
+
+" "$HOSTNAME" "$IPADDR" "$UPTIME" "$FREEFL" "$SYS_LOAD" "$CLIENTS"
