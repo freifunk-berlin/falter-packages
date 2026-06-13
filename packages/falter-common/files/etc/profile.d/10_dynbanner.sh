e401509048f699e818d614e7f3ace935a73c98b0
AnnieYousar <a.yousar@informatik.hu-berlin.de>
2026-06-13 16:09:05 +0200

    Enhance dynamic banner with system information
    Updated the dynamic banner script to include additional information as FREIFUNK_RELEASE, FREIFUNK_REVISION, DISTRIB_TARGET an all the other information already there before. Added also the hardware model.
    Instead of setting variables with dynamic information, the system commands are integrated in the printf arguments.

--- packages/falter-common/files/etc/profile.d/10_dynbanner.sh
+++ packages/falter-common/files/etc/profile.d/10_dynbanner.sh
@@ -12,20 +12,33 @@ SUFFIX=$(uci -q get profile_${COMMUNITY}.profile.suffix)
 if [ 1 -eq $? ]; then
     SUFFIX="ff"
 fi
-HOSTNAME=$(uci -q get system.@system[0].hostname)".${SUFFIX}"
-IPADDR=$(uci -q get network.dhcp.ipaddr)
-UPTIME=$(uptime | cut -d ',' -f 0 | cut -d ' ' -f 4-) >/dev/null 2>&1
-FREEFL=$(df -h | grep " /overlay" | sed -E -e s/[[:space:]]+/\;/g | cut -d';' -f4) >/dev/null 2>&1
-SYS_LOAD=$(cut -d' ' -f 1-3 </proc/loadavg) >/dev/null 2>&1
-CLIENTS=$(wc -l /tmp/dhcp.leases | cut -d' ' -f1) >/dev/null 2>&1
 
-printf \
-    " Host.............................: %s
+printf "\
+ Falter %s (%s) %s
+ https://wiki.freifunk.net/Berlin:Firmware
+ https://github.com/freifunk-berlin/falter-packages
+ -----------------------------------------------------\n
+ If you find bugs please report them at:\n
+   https://github.com/freifunk-berlin/falter-packages/issues/\n
+ For questions write a mail to <berlin@berlin.freifunk.net>
+ or check https://berlin.freifunk.net/contact for our weekly meetings.
+
+ Host.............................: %s
+ Hardware.........................: %s
  IP-Address.......................: %s
  Uptime...........................: %s
  Free flash.......................: %s
  Average load (1m, 5m, 15m).......: %s
  DHCP-Clients.....................: %s
 
-
-" "$HOSTNAME" "$IPADDR" "$UPTIME" "$FREEFL" "$SYS_LOAD" "$CLIENTS"
+" \
+ "$(sed -n "s/'//g;s/^FREIFUNK_RELEASE=//p" /etc/freifunk_release)"\
+ "$(sed -n "s/'//g;s/^FREIFUNK_REVISION=//p" /etc/freifunk_release)"\
+ "$(sed -n "s/'//g;s/^DISTRIB_TARGET=//p" /etc/openwrt_release)"\
+ "$(uci -q get system.@system[0].hostname).${SUFFIX}"\
+ "$(cat /tmp/sysinfo/model)"\
+ "$(uci -q get network.dhcp.ipaddr)"\
+ "$(uptime | sed 's/^.*up *//;s/,.*$//')"\
+ "$(df -h | grep ' /overlay' | tr -s ' ' | cut -d ' ' -f 4)"\
+ "$(uptime | sed 's/.*: //')"\
+ "$(wc -l /tmp/dhcp.leases | cut -d ' ' -f 1)"
