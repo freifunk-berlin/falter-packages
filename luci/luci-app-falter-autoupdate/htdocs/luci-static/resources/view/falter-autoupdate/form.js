f972a42a2451eb7e62402a1627ea7581ebb4aec8
Packet Please <pktpls@systemli.org>
2026-06-24 01:25:17 +0200

    fixup: autoupdate: make autoupdate.json URL configurable
    Forgot to update the Luci UI back then.
    
    Skipping the translations update for now.
    
    Signed-off-by: Packet Please <pktpls@systemli.org>
    (cherry picked from commit fd893d0e0958b16ac0d9e0ff8c2da17e0e5dc8b7)
    (cherry picked from commit 5e259a351551ed4f947f3bc0e2c24468ea6d4941)

--- luci/luci-app-falter-autoupdate/htdocs/luci-static/resources/view/falter-autoupdate/form.js
+++ luci/luci-app-falter-autoupdate/htdocs/luci-static/resources/view/falter-autoupdate/form.js
@@ -20,11 +20,9 @@ return view.extend({
     s = m.section(form.TypedSection, 'generic', _('Settings'));
     s.anonymous = true;
 
-    s.option(form.Value, 'selector_fqdn', _('Selector-FQDN'),
-      _('URL of the firmware-selector without protocol. Usally: selector.berlin.freifunk.net'));
-
-    s.option(form.Value, 'fw_server_fqdn', _('Firmware-Server-FQDN'),
-      _('URL of the firmware server without protocol. Usally firmware.berlin.freifunk.net'));
+    o1 = s.option(form.Value, 'url', _('URL'),
+      _('URL of the autoupdate.json metadata file. Usally: https://firmware.berlin.freifunk.net/stable/autoupdate.json'));
+    o1.datatype = 'string';
 
     s.option(form.Value, 'minimum_certs', _('Minimum Certs'),
       _('Minimum amount of certificates that must be valid. Otherwise autoupdate will not perform an upgrade.'));
