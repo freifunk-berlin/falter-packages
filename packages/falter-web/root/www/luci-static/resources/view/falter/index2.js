0e092250df966e37b15b73070643d2ccfbd78379
Packet Please <pktpls@systemli.org>
2026-08-08 02:51:17 +0200

    falter-web: detect password absence
    Signed-off-by: Packet Please <pktpls@systemli.org>

--- packages/falter-web/root/www/luci-static/resources/view/falter/index2.js
+++ packages/falter-web/root/www/luci-static/resources/view/falter/index2.js
@@ -8,7 +8,25 @@ const callFalterStatus = rpc.declare({
   expect: { '': {} }
 });
 
+// We want to use our own user flow for setting the initial root password,
+// but there seems to be no elegant way to detect password absence.
+//
+// So we look at the warning messages rendered by the header.ut template.
+// We could match on the string "password", but virtually all translations
+// do translate this word to something else. The other possible warning message
+// always contains the string "initramfs" though, so we can match on that.
+//
+// We assume password absence if there's any warning that's not about initramfs.
+function isPasswordAbsent() {
+  const warnings = document
+    .querySelectorAll('#maincontent>.warning h4')
+    .values();
+  return !!Array.from(warnings)
+    .find((e) => !e.innerHTML.includes('initramfs'));
+}
+
 return view.extend({
+  passwordAbsent: isPasswordAbsent(),
 
   load() {
     return L.resolveDefault(callFalterStatus());
@@ -18,6 +36,7 @@ return view.extend({
     return E([], [
       E('h1', {}, _('Hello, world!')),
       E('pre', {}, `status: ${falterStatus.status}`),
+      E('pre', {}, `passwordAbsent: ${(this.passwordAbsent)}`)
     ]);
   },
 
