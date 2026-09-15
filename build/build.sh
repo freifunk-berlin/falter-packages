80001b5ce5eb3a53d070d9479e0619ef9dd434ae
Packet Please <pktpls@systemli.org>
2026-09-15 23:23:04 +0200

    build: skip OpenWrt packages feed
    We don't seem to have build dependencies on any package
    from the OpenWrt packages feed.
    
    That means we don't need to clone packages.git.
    
    Signed-off-by: Packet Please <pktpls@systemli.org>

--- build/build.sh
+++ build/build.sh
@@ -123,14 +123,12 @@ unbuf="stdbuf --output=0 --error=0"
   if [[ "$gitmirror" =~ "git.openwrt.org" ]]; then
     cat <<EOF >"$sdkdir/feeds.conf"
 src-git base $gitmirror/openwrt/openwrt.git;$owbranch
-src-git packages $gitmirror/feed/packages.git;$owbranch2
 src-git luci $gitmirror/project/luci.git;$owbranch2
 src-link falter $(pwd)/tmp/feed
 EOF
   else
     cat <<EOF >"$sdkdir/feeds.conf"
 src-git base $gitmirror/openwrt.git;$owbranch
-src-git packages $gitmirror/packages.git;$owbranch2
 src-git luci $gitmirror/luci.git;$owbranch2
 src-link falter $(pwd)/tmp/feed
 EOF
